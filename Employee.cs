3ed272d1dcca364abf482e90707da3dc0cc41220
heejin <heuijin.ko@gmail.com>
2020-03-16 18:31:52 -0400

    .

---
+++ Employee.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementConsole
+{
+    /// <summary>
+    /// Interface Employee
+    /// </summary>
+    public interface IEmployee
+    {
+        void AddEmployee(string name, int status);   //add employee
+        void DeleteEmployee(int id);   //delete employee
+        List<Employee> GetActiveEmployee();     //get active employee
+        List<Employee> GetTerminatedEmployee(DateTime dtOfStart, DateTime dtOfEnd);     //get terminated employees
+        List<Employee> GetEmployee();   //get Currunt Employee
+    }
+
+    public class Employee : IEmployee
+    {
+        public enum actStatus { Aactive = 1, Terminated, Layoff, Leave, Quit };
+        public static int EmployeeID = 0;
+
+        private int id;
+        private string empName;
+        private int status;
+        private DateTime dtOfTerminated;
+        private List<Employee> empList;
+
+        public int Id { get => id; set => id = value; }
+        public string EmpName { get => empName; set => empName = value; }
+        public int Status { get => status; set => status = value; }
+        public DateTime DtOfTerminated { get => dtOfTerminated; set => dtOfTerminated = value; }
+
+        /// <summary>
+        /// constructor
+        /// create a new instance of list of employee 
+        /// </summary>
+        public Employee()
+        {
+            empList = new List<Employee>();
+        }
+        
+        /// <summary>
+        /// Returen list of all current employee
+        /// </summary>
+        /// <returns></returns>
+        public List<Employee> GetEmployee()
+        {
+            return empList;
+        }
+
+        /// <summary>
+        /// Return list of current active employee
+        /// </summary>
+        /// <returns></returns>
+        public List<Employee> GetActiveEmployee()
+        {
+            var empActGroug = from emp in empList
+                              where emp.status == (int)actStatus.Aactive
+                              select emp;
+
+            return empActGroug.ToList();
+        }
+
+        /// <summary>
+        /// Return list of terminated employee defend on the date
+        /// </summary>
+        /// <param name="dtOfStart">date of start</param>
+        /// <param name="dtOfEnd">date of end</param>
+        /// <returns></returns>
+        public List<Employee> GetTerminatedEmployee(DateTime dtOfStart, DateTime dtOfEnd)
+        {
+            var empActGroug = from emp in empList
+                              where emp.dtOfTerminated >= dtOfStart &&
+                               emp.dtOfTerminated <= dtOfEnd
+                              select emp;
+
+            return empActGroug.ToList();
+        }
+
+        /// <summary>
+        /// Add employee
+        /// </summary>
+        /// <param name="newName"></param>
+        /// <param name="newStatus"></param>
+        public virtual void AddEmployee(string newName, int newStatus)
+        {
+            Employee e1 = new Employee();
+
+            e1.id = ++EmployeeID;
+            e1.empName = newName;
+            e1.status = newStatus;
+
+            empList.Add(e1);
+            Console.WriteLine($"Added ID: {EmployeeID}, Employee Name: {newName}, status: {GetStatus(newStatus)}");
+        }
+
+        /// <summary>
+        /// Add employee
+        /// </summary>
+        /// <param name="emp"></param>
+        public virtual void AddEmployee(Employee emp)
+        {
+            empList.Add(emp);
+            Console.WriteLine($"ID: {emp.id}, Employee Name: {emp.empName}, status: {GetStatus(emp.status)}");
+            Console.WriteLine($"Added a new employee\n");
+        }
+
+        /// <summary>
+        /// Remove employee by id
+        /// </summary>
+        /// <param name="id"></param>
+        public virtual void DeleteEmployee(int id)
+        {
+            empList.RemoveAll(x => x.id == id);
+        }
+
+        /// <summary>
+        /// Set date of terminate for the test
+        /// </summary>
+        /// <param name="id">employee id</param>
+        /// <param name="dt">date of terminated</param>
+        public virtual void SetTerminatedDate(int id, string dt)
+        {
+            var emp = empList.First(i => i.id == id);
+            var index = empList.IndexOf(emp);
+
+            if (index != -1) { 
+                empList[index].dtOfTerminated = DateTime.Parse(dt);
+                empList[index].status = (int)actStatus.Terminated;
+            }
+        }
+
+        /// <summary>
+        /// convert enum variable to string
+        /// </summary>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        private string GetStatus(int status)
+        {
+            string strStatus = "";
+            switch (status)
+            {
+                case (int)actStatus.Aactive:
+                    strStatus = "Active";
+                    break;
+                case (int)actStatus.Terminated:
+                    strStatus = "Terminated";
+                    break;
+                case (int)actStatus.Layoff:
+                    strStatus = "Layoff";
+                    break;
+                case (int)actStatus.Leave:
+                    strStatus = "Leave";
+                    break;
+                case (int)actStatus.Quit:
+                    strStatus = "Quit";
+                    break;
+                default:
+                    break;
+            }
+
+            return strStatus;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Add Employee
+             *    Employee ID is increse automatically.
+             *    Can set current status of employee
+            */
+
+            Console.WriteLine($"\nAdd Employees:");
+            Employee e1 = new Employee();
+            e1.AddEmployee("Terry", (int)Employee.actStatus.Aactive);
+            e1.AddEmployee("Cesar", (int)Employee.actStatus.Aactive);
+            e1.AddEmployee("Debra", (int)Employee.actStatus.Layoff);
+            e1.AddEmployee("Hugo", (int)Employee.actStatus.Aactive);
+            e1.AddEmployee("Claire", (int)Employee.actStatus.Aactive);
+            e1.AddEmployee("Eugene", (int)Employee.actStatus.Aactive);
+            e1.AddEmployee("Steve", (int)Employee.actStatus.Leave);
+            e1.AddEmployee("Candy", (int)Employee.actStatus.Aactive);
+
+            Console.WriteLine($"\nCurrunt Employees:");
+            foreach (Employee aEmployee in e1.GetEmployee())
+            {
+                Console.WriteLine($"ID: {aEmployee.Id}, Name: {aEmployee.EmpName}");
+            }
+
+            /*
+             * 2. Remove employee by Employee ID
+            */
+
+            e1.DeleteEmployee(2);
+            Console.WriteLine($"\nDelete a employee: ID:{2}");
+            foreach (Employee aEmployee in e1.GetEmployee())
+            {
+                Console.WriteLine($"ID: {aEmployee.Id}, Name: {aEmployee.EmpName}");
+            }
+
+
+            /*
+             * 3. Return a list of active employees
+            */
+            Console.WriteLine($"\nActive Employees:");
+            var actEmp = e1.GetActiveEmployee();
+            foreach (Employee aEmployee in actEmp)
+            {
+                Console.WriteLine($"ID: {aEmployee.Id}, Name: {aEmployee.EmpName}");
+            }
+
+
+            /*
+             * Set terminate date for test
+            */
+            Console.WriteLine($"\nSet date of terminated:");
+            e1.SetTerminatedDate(3, "12/21/2019");
+            e1.SetTerminatedDate(5, "02/21/2020");
+            e1.SetTerminatedDate(6, "02/21/2020");
+            e1.SetTerminatedDate(7, "03/21/2020");
+
+            var tEmp = from emp in e1.GetEmployee()
+                     where emp.Status == (int)Employee.actStatus.Terminated
+                     select emp;
+
+            foreach (Employee aEmployee in tEmp)
+            {
+                
+                Console.WriteLine($"ID: {aEmployee.Id}, Name: {aEmployee.EmpName}, " +
+                    $"set terminated date: {String.Format("{0:MM/dd/yyyy}", aEmployee.DtOfTerminated)}");
+            }
+
+            /*
+             *  4. Return a list of terminated employees in the last month
+            */
+            Console.WriteLine($"\nGet employee of terminated in the last month:");
+            var today = DateTime.Today;
+            var month = new DateTime(today.Year, today.Month, 1);
+            var first = month.AddMonths(-1);
+            var last = month.AddDays(-1);
+
+            var firedEmp = e1.GetTerminatedEmployee(first, last);
+                              
+            foreach (Employee aEmployee in firedEmp)
+            {
+                Console.WriteLine($"ID: {aEmployee.Id}, Name: {aEmployee.EmpName}, " +
+                    $"terminated date: {String.Format("{0:MM/dd/yyyy}", aEmployee.DtOfTerminated)}");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// Get information of employee from Front form if applicable
+        /// </summary>
+        /// <returns></returns>
+        private static Employee AddEmployee()
+        {
+            Employee e1 = new Employee();
+            e1.EmpName = "Terry";
+            e1.Status = (int)Employee.actStatus.Aactive;
+
+            return e1;
+        }
+    }
+}
